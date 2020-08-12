c73ef14e2f2da529b4721a40fb8ef5c73ad5b026
Eugene Fedoseev <abap@sapville.com>
2020-08-12 22:54:42 +0000

    simple promise

--- js/init.js
+++ js/init.js
@@ -4,18 +4,36 @@ function showResult() {
     document.getElementById('paragraph1').innerHTML = sText;
   }
 
-  const oReq = new XMLHttpRequest();
-  const sAppID = 'da1240fb8032e8e258bc11509bc2ce04';
-  // const sAppID = '';
-  oReq.addEventListener('load', function() {
-    if (this.status === 200) {
-      showText(JSON.parse(this.responseText).main.temp);
-    }
-    else {
-      showText('Error happened: ' + this.status + ' ' + this.statusText);
-    }
-  });
-  oReq.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=Vancouver&units=metric&appid=' + sAppID);
-  oReq.send();
+  function getTemperature() {
+    return new Promise((resolve, reject) => {
+      const oReq = new XMLHttpRequest();
+      const sAppID = 'da1240fb8032e8e258bc11509bc2ce04';
+      // const sAppID = '';
+      oReq.addEventListener('load', function() {
+        if (this.status === 200) {
+          resolve(this);
+        }
+        else {
+          reject(this);
+        }
+      });
+      oReq.addEventListener('error', function() {
+        reject(this);
+      });
+      oReq.addEventListener('timeout', function() {
+        reject(new function() {
+          this.status = 'failed';
+          this.statusText = 'Time Out';
+        });
+      });
+      oReq.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=Vancouver&units=metric&appid=' + sAppID);
+      oReq.send();
+
+    });
+
+  }
+  getTemperature()
+    .then(result => showText(JSON.parse(result.responseText).main.temp))
+    .catch(error => showText('Error happened: ' + error.status + ' ' + error.statusText));
   showText('Wait for this...');
 }
